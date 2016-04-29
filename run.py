999a20f55569f48e9705e357be6871006043d353
Joshua Haas <haasj74@students.rowan.edu>
2016-04-28 22:34:25 -0400

    updates default config and run.py

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
-from sibylbot import SibylBot
+import sys
 
-bot = SibylBot()
+from sibyl.sibylbot import SibylBot
+
+conf = 'sibyl.conf'
+if len(sys.argv)>1:
+  conf = sys.argv[1]
+
+bot = SibylBot(conf)
 bot.run_forever()
