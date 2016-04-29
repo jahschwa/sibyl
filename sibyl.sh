4663ab278b6e9e636562ae21f385f962d0a7f560
Joshua Haas <haasj74@students.rowan.edu>
2016-04-29 15:15:34 -0400

    actually fixes sibyl.sh

--- sibyl.sh
+++ sibyl.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-(setsid python run.py & echo $! > /var/run/sibyl/sibyl.pid)
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
+(setsid python sibyl/run.py & echo $! > /var/run/sibyl/sibyl.pid)
