0518969f260782bdb4d52ffb67400be3eb1c5128
Joshua Haas <haasj74@students.rowan.edu>
2016-04-29 15:13:47 -0400

    fixes cd in sibyl.sh

--- sibyl.sh
+++ sibyl.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-(setsid python "$dir/run.py" & echo $! > /var/run/sibyl/sibyl.pid)
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+(setsid python run.py & echo $! > /var/run/sibyl/sibyl.pid)
