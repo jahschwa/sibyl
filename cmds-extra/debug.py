513124836185ded1eb6cb76d681509e6114f4c6e
Joshua Haas <haasj74@students.rowan.edu>
2017-02-19 17:17:27 -0500

    add disclaimer

--- cmds-extra/debug.py
+++ cmds-extra/debug.py
@@ -1,3 +1,15 @@
+# WARNING: USE AT YOUR OWN RISK
+#
+# These commands implicitly allow for arbitrary code execution.
+#
+# They are only recommended for use on local testing instances.
+#
+# If you really want to have them on an internet-facing Sibyl, then at least
+# restrict them in bw_list in your config.
+#
+# The Sibyl project and its developers are not responsible for anything that
+# happens due to the use of these commands.
+
 from sibyl.lib.decorators import botcmd
 
 import logging
