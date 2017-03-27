e573a3e1b80d26e9c049bd17e2b6357798f0209a
Jonathan Frederickson <jonathan@terracrypt.net>
2017-03-27 13:15:20 -0400

    Add 5s timeout to link_echo get request
    This does not prevent sibyl from attempting to download large files,
    but it does help if the connection between sibyl and the HTTP server
    is severed without the socket being closed.

--- cmds/room.py
+++ cmds/room.py
@@ -483,7 +483,7 @@ def link_echo(bot,mess,cmd):
     if len(urls)==0:
       return
     for url in urls:
-      r = requests.get(url)
+      r = requests.get(url, timeout=5)
       title = fromstring(r.content).findtext('.//title')
       titles.append(title.strip())
     linkcount = 1
